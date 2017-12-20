8839a84b849c6f9dc0791f150e54c87467bf7950
JingXu <kyrie.jing1994@gmail.com>
2017-12-20 22:19:22 +0800

    学习了一些有关编码的知识，并做了相关练习

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,20 @@
-print('hello world')
-print('The quick brown fox','jumps over','the lazy dog')
-print('I\'m \"ok\"')
-print('I\'m learning\nPython.')
-print(r'\\\t\\')
+# -*- coding:utf-8 -*-
+#------------------------------------------------------------
+# print('hello world')
+# print('The quick brown fox','jumps over','the lazy dog')
+# print('I\'m \"ok\"')
+# print('I\'m learning\nPython.')
+# print(r'\\\t\\')
+#------------------------------------------------------------
+# print absolute value of an integer 
+# a=100 
+# if a>=0:
+#     print(a)
+# else:
+#     print(-a)
+#------------------------------------------------------------
+s='Python-中文'
+print(s)
+b=s.encode('utf-8')
+print(b)
+print(b.decode('utf-8'))
