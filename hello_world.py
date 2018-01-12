d342d615472494270d0c7764d6b1794025e66f6d
JingXu <kyrie.jing1994@gmail.com>
2018-01-12 19:53:11 +0800

    学习了day03以及day04的部分内容

--- hello_world.py
+++ hello_world.py
@@ -13,8 +13,205 @@
 # else:
 #     print(-a)
 #------------------------------------------------------------
-s='Python-中文'
-print(s)
-b=s.encode('utf-8')
-print(b)
-print(b.decode('utf-8'))
+# s='Python-中文'
+# print(s)
+# b=s.encode('utf-8')
+# print(b)
+# print(b.decode('utf-8'))
+#------------------------------------------------------------
+# age = 3
+# if age >= 18:
+#     print('Your age is', age)
+#     print('adult')
+# else:
+#     print('Your age is', age)
+#     print('teenager')
+#------------------------------------------------------------
+# sum = 0
+# for x in range(101):
+#     sum = sum + x
+# print(sum)
+#------------------------------------------------------------
+# sum = 0
+# n = 99
+# while n > 0:
+#     sum = sum + n 
+#     n = n - 2
+# print(sum)
+#------------------------------------------------------------
+import math
+def quadratic(a, b, c):
+    delta = math.sqrt( b*b - 4*a*c )
+    x1=( -b + delta )/( 2*a )
+    x2=( -b - delta )/( 2*a )
+    return x1,x2
+
+r=quadratic(2,3,1)
+print(r) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
