499066ad3156d5de1468e32e8075897a0203f191
JingXu <kyrie.jing1994@gmail.com>
2018-01-12 20:50:44 +0800

    完成了day04的学习，练习了while循环，用两种循环方式完成了打印乘法表的任务

---
+++ day0304.py
@@ -0,0 +1,139 @@
+# -----------------------------------------------------------------------------------------------------------------
+# print(3*4*5/2)
+# x = 3
+# y = 4
+# print("x乘以y=",x*y)
+# -----------------------------------------------------------------------------------------------------------------
+# Variables变量 are used to store保存、储存 information信息 to be referenced被日后调用 and manipulated操作或更改
+# in a computer program程序. They also并且还 provide提供 a way方式 of labeling标记 data数据 with a descriptive描述性
+#  name, so our programs can be understood理解 more clearly更清晰 by the reader阅读者 and ourselves我们自己.
+#  It is helpful to think of variables as containers容器 that hold保持、保存 information
+# (如果我们把变量看作成是一个保存信息的容器是更容易理解的). Their sole主要 purpose目的 is to label标记 and store存储
+#  data in memory内存里. This data数据 can then然后 be used使用它 throughout整个 your program
+# -----------------------------------------------------------------------------------------------------------------
+# student_number = 30     #Python
+# studentNumber = 30      #驼峰体
+# -----------------------------------------------------------------------------------------------------------------
+# 利用flag
+# age_of_princal = 56
+# flag = True
+# while flag:
+#     guess_age = int( input(">>:") )
+#     if guess_age == age_of_princal:
+#         print("Yes,you got it...")
+#         flag = False
+#     elif guess_age > age_of_princal:
+#         print("You should try smaller...")
+#     else:
+#         print("You should try bigger...")
+# print("End")
+# -----------------------------------------------------------------------------------------------------------------
+# 利用break语句
+# age_of_princal = 56
+# while True:
+#     guess_age = int( input(">>:") )
+#     if guess_age == age_of_princal:
+#         print("Yes,you got it...")
+#         break
+#     elif guess_age > age_of_princal:
+#         print("You should try smaller...")
+#     else:
+#         print("You should try bigger...")
+# print("End")
+# -----------------------------------------------------------------------------------------------------------------
+# height = int(input("Height:"))
+# width = int(input("Width:"))
+# num_height = 1
+# while num_height <= height:
+# 	num_width = 1
+# 	while num_width <= width:
+# 		print("#", end="")
+# 		num_width += 1
+# 	print()
+# 	num_height += 1
+# -----------------------------------------------------------------------------------------------------------------
+# for i in range(9):
+# 	i += 1
+# 	for j in range(i):
+# 		j += 1
+# 		print(str(j)+"*"+str(i)+"="+str(j*i) , end="\t")
+# 	print()
+# -----------------------------------------------------------------------------------------------------------------
+# first = 1
+# while first <= 9:
+# 	sec = 1
+# 	while sec <= first:
+# 		print( str(sec) + "*" + str(first) + "=" + str(sec*first) , end="\t" )
+# 		sec += 1
+# 	print()
+# 	first += 1
+# -----------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
