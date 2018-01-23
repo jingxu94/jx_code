e93c20863e48711e45b6dff488fe3dbd0f63071e
JingXu <kyrie.jing1994@gmail.com>
2018-01-23 10:38:46 +0800

    修改了正则计算器代码，对比eval函数验证了其正确性，现可实现对带括号的加减乘除乘方表达式计算

--- fullstack_s2/day19/Calculator.py
+++ fullstack_s2/day19/Calculator.py
@@ -2,17 +2,33 @@
 #date:  2018/1/22
 
 import re
+import sys
 
-source = " 1- 2 *(( 60-30+(-40/5)* ( 9-2*5/3+7/3*99/4*2998 + 10*568 /14))-( -4*3) / ( 16-3*2)) "
 
-def check(string):
-	flag = True
-	if re.findall('[a-zA-Z]',string):
-		print('Invalid')
-		flag = False
-	return flag
+def start_window():
+	'''
+	起始界面
+	:param:	expression:待计算表达式
+	:return: 返回有内容的输入
+	'''
+	start_info = "正则计算器"
+	print( start_info.center( 50, "*" ), "\n" )
+	while True:
+		expression = input(  "请输入你要计算的表达式[q:退出]：\n" ).strip()
+		if expression == "q" or expression == "Q":
+			sys.exit("关闭计算器")
+		elif len(expression) == 0:
+			continue
+		else:
+			return expression
+
 
 def format_string(string):
+	'''
+	格式整理，包括正负关系判断和去除空格
+	:param string: 待整理格式的表达式
+	:return: 返回去除多余正负号和空格的表达式
+	'''
 	string = string.replace('--', '+')
 	string = string.replace('-+', '-')
 	string = string.replace('+-', '-')
@@ -22,8 +38,14 @@ def format_string(string):
 	string = string.replace(' ', '')
 	return string
 
+
 def cal_mul_div(string):
-	regular = '[\-]\d+\.?\d*[*/][\-]\d+\.?\d*'
+	'''
+	乘除乘方运算
+	:param string: 待计算表达式，不包含括号
+	:return: 返回计算完全部乘除和乘方运算的表达式
+	'''
+	regular = '\d+\.*\d*[*/]+[\+\-]?\d+\.*\d*'
 
 	while re.findall( regular, string ):
 		expression = re.search( regular, string ).group()
@@ -36,11 +58,14 @@ def cal_mul_div(string):
 
 		if expression.count("/"):
 			x, y = expression.split("/")
+			if float(y) == 0:
+				print("计算部分有0作分母，请重新检查输入！")
+				start_window()
 			div_result = str( float(x) / float(y) )
 			string = string.replace( expression, div_result )
 			string = format_string(string)
 
-		if expression.count("**") == 2:
+		if expression.count("*") == 2:
 			x, y = expression.split("**")
 			pow_result = 1
 			for i in range(int(y)):
@@ -50,8 +75,12 @@ def cal_mul_div(string):
 	return string
 
 
-
 def cal_add_sub(string):
+	'''
+	加减运算
+	:param string:待计算表达式，不包含括号和乘除运算
+	:return: 返回计算完全部加减运算的表达式
+	'''
 	add_regular = "[\-]?\d+\.?\d*\+[\-]?\d+\.?\d*"
 	sub_regular = "[\-]?\d+\.?\d*\-[\-]?\d+\.?\d*"
 
@@ -81,8 +110,12 @@ def cal_add_sub(string):
 	return string
 
 
-
-if check(source):
+def recalculate(source):
+	'''
+	正则计算器
+	:param source:待计算表达式
+	:return: 返回正则计算器计算结果，输出为字符型
+	'''
 	source = format_string(source)
 	while source.count('(') > 0:
 		strs = re.search( '\([^()]+\)', source ).group()
@@ -93,4 +126,23 @@ if check(source):
 		replace_str = cal_mul_div(source)
 		replace_str = cal_add_sub(replace_str)
 		source = source.replace( source, replace_str )
-	print( "my result:", source.replace( "+", "" ) )
+	return source.replace( "+", "" )
+
+
+if __name__ == "__main__":
+	try:
+		expression = start_window()
+		reresult = recalculate( expression )
+		result = eval( expression )
+		reresult = float( reresult )
+		result = float( result )
+		if result == reresult:
+			print( "eval计算结果：%s" % result )
+			print( "正则计算器计算结果：%s" % reresult )
+		else:  # 两种计算方式的结果不正确，提示异常，并返回两种方式的计算结果
+			print( "计算结果异常，请重新检查输入！")
+			print( "eval计算结果：%s" % result )
+			print( "正则计算器计算结果：%s" % reresult )
+	except(SyntaxError, ValueError, TypeError):  # 如果有不合法输出，则抛出错误
+		print("输入表达式不合法，请重新检查输入！")
+		start_window()
